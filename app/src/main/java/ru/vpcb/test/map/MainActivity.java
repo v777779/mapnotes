6b6f69460b978720c445a81c3d028e28d650734e
Vadim Voronov <vadim.v.voronov@gmail.com>
2019-05-07 19:01:16 +0300

    project init

---
+++ app/src/main/java/ru/vpcb/test/map/MainActivity.java
@@ -0,0 +1,66 @@
+package ru.vpcb.test.map;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Replace with your own action",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_login);
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    return true;
+
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_maps);
+                    startActivity(new Intent(MainActivity.this, MapsActivity.class));
+                    return true;
+            }
+            return false;
+        }
+    };
+}
