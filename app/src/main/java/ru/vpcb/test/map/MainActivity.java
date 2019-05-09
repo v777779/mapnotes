997095dcba7a8a7868d9c74538bb59393958a58f
Vadim Voronov <vadim.v.voronov@gmail.com>
2019-05-09 11:39:04 +0300

    project init firestore added

--- app/src/main/java/ru/vpcb/test/map/MainActivity.java
+++ app/src/main/java/ru/vpcb/test/map/MainActivity.java
@@ -17,11 +17,19 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
+
+import javax.annotation.Nullable;
 
 import ru.vpcb.test.map.data.IJob;
 import ru.vpcb.test.map.data.Result;
@@ -42,16 +50,15 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-        if(sHandler == null)sHandler = new Handler();
+        if (sHandler == null) sHandler = new Handler();
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Toast.makeText(MainActivity.this, "Replace with your own action",
-//                        Toast.LENGTH_SHORT).show();
-
                 init();
+//                database();
+//                firestore();
             }
         });
 
@@ -89,7 +96,6 @@ public class MainActivity extends AppCompatActivity {
     private void init() {
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         String notesPath = "notes";
-
 
         IJob<Note> replaceAuthorName = new IJob<Note>() {
             @Override
@@ -135,8 +141,8 @@ public class MainActivity extends AppCompatActivity {
 
                 sync.waiting();
                 Result<List<Note>> result = sync.getResult();
-                sHandler.post(() -> Toast.makeText(MainActivity.this,"note: "+
-                                ((Result.Success<List<Note>>)result).getData().get(0).getUser(),
+                sHandler.post(() -> Toast.makeText(MainActivity.this, "note: " +
+                                ((Result.Success<List<Note>>) result).getData().get(0).getUser(),
                         Toast.LENGTH_SHORT).show());
                 int k = 1;
             }
@@ -145,6 +151,79 @@ public class MainActivity extends AppCompatActivity {
 
         int k = 1;
     }
+
+
+// database
+
+    private void database() {
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        String notesPath = "notes";
+
+        IJob<Note> replaceAuthorName = new IJob<Note>() {
+            @Override
+            public void join(Note note) {
+
+                if (note == null) return;
+                note.setUser(note.getUser());
+            }
+        };
+
+        database.getReference(notesPath).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.exists()) {
+                    List<Note> noteResults = new ArrayList<>();
+                    for (DataSnapshot child : dataSnapshot.getChildren()) {
+                        Note note = child.getValue(Note.class);
+                        replaceAuthorName.join(note);
+                        noteResults.add(note);
+                    }
+                    Result<List<Note>> result = new Result.Success<>(noteResults);
+                    String s = ((Result.Success<List<Note>>) result).getData().get(0).getUser();
+                    Toast.makeText(MainActivity.this, "note: " + s, Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Result<List<Note>> result = new Result.Error<>(new NullPointerException());
+                    String s = ((Result.Error<List<Note>>) result).getException().toString();
+                    Toast.makeText(MainActivity.this, "note: " + s, Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Result<List<Note>> result = new Result.Error<>(databaseError.toException());
+                String s = ((Result.Error<List<Note>>) result).getException().toString();
+                Toast.makeText(MainActivity.this, "note: " + s, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+// firestore
+
+    private void firestore(){
+        FirebaseFirestore fireStore = FirebaseFirestore.getInstance();
+        CollectionReference reference = fireStore.collection("mapnotes");
+        reference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                if(e == null) {
+                    List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                    String s = list.get(0).getData().toString();
+                    Toast.makeText(MainActivity.this, "note: " + s, Toast.LENGTH_SHORT).show();
+                }else {
+                    String s = e.toString();
+                    Toast.makeText(MainActivity.this, "note: " + s, Toast.LENGTH_SHORT).show();
+                }
+
+                int k = 1;
+            }
+        });
+    }
+
+// classes
 
     private static class Sync<T> {
         private final Object lock;
