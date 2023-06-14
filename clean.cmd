510a812a8ffca713441b665eb7b11d6f4b3a386d
v1 <free2022@vpcb.ru>
2023-06-14 20:16:20 +0300

    map files update#

--- clean.cmd
+++ clean.cmd
@@ -6,3 +6,5 @@ rmdir /q/s build
 rmdir /q/s app\build
 del /q  *.iml *.apks app\*.iml local.properties
 rem del /q  gradlew* *.iml  app\*.iml local.properties
+
+copy /y ..\copy_notes\original\build.gradle app\build.gradle
---
+++ restore.cmd
@@ -0,0 +1,3 @@
+
+copy /y ..\copy_notes\local\build.gradle app\build.gradle
+pause
